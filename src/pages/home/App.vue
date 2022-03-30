6b6939cc74891e25f9b33efede56a03a90e8fe0d
destroylord <masapin68@gmail.com>
2022-03-30 21:23:32 +0700

    add 2 row

--- src/pages/home/App.vue
+++ src/pages/home/App.vue
@@ -1,3 +1,10 @@
 <template>
-    <h1>Hello World</h1>
+    <div class="row">
+        <div class="col-md-6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit doloribus atque incidunt sint voluptate, itaque soluta hic placeat cumque nostrum. Inventore impedit ad sit vel quisquam unde similique non eligendi?
+        </div>
+        <div class="col-md-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatum eveniet repellat facilis optio perferendis, enim commodi minima quas ex velit, deleniti odit. Tempora quam explicabo reprehenderit earum dignissimos cumque?
+        </div>
+    </div>
 </template>
