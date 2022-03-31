1155c37c40490897c9939d047fcf44031b890ea2
nilamalia02 <nilaa3794@gmail.com>
2022-03-31 11:41:10 +0700

    finished

--- src/pages/market/App.vue
+++ src/pages/market/App.vue
@@ -1,6 +1,5 @@
 <template>
-<link rel="stylesheet" href="assets/css/custom.css">
- <section class="section bg-light" id="plans">
+ <section class="section " id="plans">
             <div class="bg-overlay bg-overlay-pattern"></div>
             <div class="container">
                 <div class="row justify-content-center">
@@ -12,7 +11,7 @@
 
                             <div class="d-flex align-items-center">
                                 <div>
-                                    <a href="index.html" class="btn btn-primary btnmarket">Buka Kopdig Market </a>
+                                    <button href="index.html" style="background-color: #95AE36 ; border:#95AE36" class="btn-primary btnmarket">Buka Kopdig Market </button>
                                 </div>
                             </div>
                         </div>
@@ -23,63 +22,67 @@
                     <!-- end col -->
                 </div>
                 <!-- end row -->
-            <h3 class="mb-3 text">Fitur Kopding <span class="txt1">Market </span></h3>
+            <h3 class="mb-3 text"><b>Fitur Kopding <span class="txt1">Market </span></b></h3>
+            <div class="row">
+            <div class="col-lg-6">
             <div class="crd2">
                 <div class="row gy-4">
-                    <div class="col-lg-6">
-                        <div class="card plan-box mb-0">
+                            <div class="crd1">
                             <div class="card-body p-4 m-2">
                                 <div class="d-flex align-items-center">
                                     <div class="flex-grow-1">
                                         <h5 class="mb-1 fw-semibold">Katalog Produk</h5>
                                         <p class="text-muted mb-0">Find your needs and make purchases quickly and safely</p>
-                                    
+                                  
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                        <div class="card plan-box mb-0">
-                            <div class="card-body p-4 m-2">
-                                <div class="d-flex align-items-center">
-                                    <div class="flex-grow-1">
-                                        <h5 class="mb-1 fw-semibold">Point</h5>
-                                        <p class="text-muted mb-0">Hasil Point yang didapat</p>
-                                    
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="crd">
-                    <div class="row gy-4">
-                    <div class="col-lg-6">
-                        <div class="card plan-box mb-0">
+                 
+                       
+                            <div class="crd">
                             <div class="card-body p-4 m-2">
                                 <div class="d-flex align-items-center">
                                     <div class="flex-grow-1">
                                         <h5 class="mb-1 fw-semibold">Custom Marketplace</h5>
                                         <p class="text-muted mb-0">Hasil Point yang didapat</p>
-                                    
+                                   </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                        <div class="card plan-box mb-0">
+                    </div>
+                    <div class="col-lg-6">
+            <div class="crd2">
+                <div class="row gy-4">
+                            <div class="crd1">
+                            <div class="card-body p-4 m-2">
+                                <div class="d-flex align-items-center">
+                                    <div class="flex-grow-1">
+                                        <h5 class="mb-1 fw-semibold">Point</h5>
+                                        <p class="text-muted mb-0">Hasil point yang didapat</p>
+                                  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                
+                            <div class="crd">
                             <div class="card-body p-4 m-2">
                                 <div class="d-flex align-items-center">
                                     <div class="flex-grow-1">
                                         <h5 class="mb-1 fw-semibold">Payment Gateway</h5>
                                         <p class="text-muted mb-0">Transaksi Online</p>
-                                    
+                                   </div>
                                     </div>
                                 </div>
                             </div>
-                     </div>
-                     </div>
+                        </div>
                     </div>
+                    </div>
+                    
+            </div>
                     <!--end col-->
                     
                    
@@ -89,3 +92,7 @@
             <!-- end container -->
         </section>
 </template>
+<style >
+@import "market.css";
+
+</style>
